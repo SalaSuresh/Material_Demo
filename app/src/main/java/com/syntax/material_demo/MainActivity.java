d9e7b56735a282d90e3f6c567ada40ea58d6ebfa
Suresh <suresh.sala1992@gmail.com>
2016-06-08 14:48:05 +0530

    nd bg and profile pic images added

--- app/src/main/java/com/syntax/material_demo/MainActivity.java
+++ app/src/main/java/com/syntax/material_demo/MainActivity.java
@@ -1,19 +1,106 @@
 package com.syntax.material_demo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+    FrameLayout mContentFrame;
 
+    private static final String PREFERENCES_FILE = "mymaterialapp_settings";
+    private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
+    private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+    private int mCurrentSelectedPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //test ----- START
+//        setUpToolbar();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
+
+        mUserLearnedDrawer = Boolean.valueOf(readSharedSetting(this, PREF_USER_LEARNED_DRAWER, "false"));
+
+        if (savedInstanceState != null) {
+            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+            mFromSavedInstanceState = true;
+        }
+
+        setUpNavDrawer();
+
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mContentFrame = (FrameLayout) findViewById(R.id.nav_contentframe);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                menuItem.setChecked(true);
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_item_1:
+                        Snackbar.make(mContentFrame, "Item One", Snackbar.LENGTH_SHORT).show();
+                        mCurrentSelectedPosition = 0;
+                        return true;
+                    case R.id.navigation_item_2:
+                        Snackbar.make(mContentFrame, "Item Two", Snackbar.LENGTH_SHORT).show();
+                        mCurrentSelectedPosition = 1;
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+        //test ----- END
+
     }
+
+    //test ----- START
+    private void setUpToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+    }
+
+    private void setUpNavDrawer() {
+        if (mToolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mToolbar.setNavigationIcon(R.drawable.profile_picture);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+        }
+    }
+
+    public static String readSharedSetting(Context ctx, String settingName, String defaultValue) {
+        SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        return sharedPref.getString(settingName, defaultValue);
+    }
+    //test ----- END
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

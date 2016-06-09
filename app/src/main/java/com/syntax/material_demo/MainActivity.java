88eb41ffa3e9cfeb2bff04f01a3aaba37cdb9056
Suresh <suresh.sala1992@gmail.com>
2016-06-09 18:11:23 +0530

    View Pager added in Main Fragment

--- app/src/main/java/com/syntax/material_demo/MainActivity.java
+++ app/src/main/java/com/syntax/material_demo/MainActivity.java
@@ -15,6 +15,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+/*
+Reference Links
+http://android-er.blogspot.in/2014/04/example-of-viewpager-with-custom.html
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
@@ -51,10 +56,9 @@ public class MainActivity extends AppCompatActivity {
 
 
                 //Checking if the item is in checked state or not, if not make it in checked state
-//                if(menuItem.isChecked()) menuItem.setChecked(false);
-//                else menuItem.setChecked(true);
-                menuItem.setChecked(true);
-                //Closing drawer on item click
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+                //Closing navigation_drawer_items on item click
                 drawerLayout.closeDrawers();
 
                 //Check to see which item was being clicked and perform appropriate action
@@ -105,19 +109,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                // Code here will be triggered once the navigation_drawer_items closes as we dont want anything to happen so we leave this blank
                 super.onDrawerClosed(drawerView);
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                // Code here will be triggered once the navigation_drawer_items open as we dont want anything to happen so we leave this blank
 
                 super.onDrawerOpened(drawerView);
             }
         };
 
-        //Setting the actionbarToggle to drawer layout
+        //Setting the actionbarToggle to navigation_drawer_items layout
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
         //calling sync state is necessay or else your hamburger icon wont show up
--- app/src/main/java/com/syntax/material_demo/MainFragmest.java
+++ app/src/main/java/com/syntax/material_demo/MainFragmest.java
@@ -1,21 +1,106 @@
 package com.syntax.material_demo;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by sures on 6/8/2016.
  */
 public class MainFragmest extends Fragment {
-
+    ViewPager viewPager;
+    MyPagerAdapter myPagerAdapter;
+    Activity activity = getActivity();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.main_fragment,container,false);
+        viewPager = (ViewPager)v.findViewById(R.id.myviewpager);
+        myPagerAdapter = new MyPagerAdapter();
+        viewPager.setAdapter(myPagerAdapter);
         return v;
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+
+        int NumberOfPages = 5;
+
+        int[] res = {
+                R.drawable.image_1,
+                R.drawable.image_2,
+                R.drawable.image_3,
+                R.drawable.image_4,
+                R.drawable.image_5};
+        int[] backgroundcolor = {
+                0xFF101010,
+                0xFF202020,
+                0xFF303030,
+                0xFF404040,
+                0xFF505050};
+
+        @Override
+        public int getCount() {
+            return NumberOfPages;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+
+//            TextView textView = new TextView(getActivity());
+//            textView.setTextColor(Color.WHITE);
+//            textView.setTextSize(30);
+//            textView.setTypeface(Typeface.DEFAULT_BOLD);
+//            textView.setText(String.valueOf(position));
+
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setImageResource(res[position]);
+            ViewGroup.LayoutParams imageParams = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            imageView.setLayoutParams(imageParams);
+
+            LinearLayout layout = new LinearLayout(getActivity());
+            layout.setOrientation(LinearLayout.VERTICAL);
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+//            layout.setBackgroundColor(backgroundcolor[position]);
+            layout.setLayoutParams(layoutParams);
+//            layout.addView(textView);
+            layout.addView(imageView);
+
+            final int page = position;
+            layout.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "Page " + page + " clicked", Toast.LENGTH_LONG).show();
+                }});
+
+            container.addView(layout);
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout)object);
+        }
+
     }
 }
